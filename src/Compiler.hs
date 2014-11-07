dd198371ed1bc5f71b848dcce24d976dd686c741
Andrey Demin <ademinn@gmail.com>
2014-11-08 01:50:40 +0300

    simple compiler

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,30 +1,14 @@
 module Main where
 
-import qualified System.Environment as Env
-
 import qualified Lexer as L
 
 import Checker
 import Parser
-import Control.Applicative
-import Control.Monad.State
-import Control.Monad.Writer
-import Control.Monad.Identity
-import Control.Monad.Error
-
-codeExtension :: String
-codeExtension = ".java"
-
-assemblyExtension :: String
-assemblyExtension = ".ll"
-
-liftError :: ErrorT String IO a -> IO a
-liftError = runErrorT >=> either fail return
-
-splitLast :: Int -> String -> (String, String)
-splitLast i s = splitAt (length s - i) s
+import Control.Monad.Except
 
 main :: IO ()
 main = do
     s <- getLine
-    print $ L.runAlex s parse
+    print $ case L.runAlex s parse of
+        Right p -> show . runExcept . checkProgram $ p
+        Left err -> err
